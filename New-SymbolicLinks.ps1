2234c7e40c537d54d12961171b9ddf0c284cc37a
Gonzalo Contento <gonzalo.contento@micronotes.com>
2024-11-19 12:55:14 -0500

    Refactor symbolic link creation and add new link for Material directory

--- New-SymbolicLinks.ps1
+++ New-SymbolicLinks.ps1
@@ -2,46 +2,29 @@
 
 # To Folders
 $LinkInfo = @{
-  "C:/Projects"            = "$Home/Projects";
-  "C:/Logs"                = "$Home/Logs";
-  "C:/Temp"                = "$Home/Temp";
-  "C:/Virtual Machines"    = "$Home/Virtual Machines";
-  "C:/Backup"              = "$Env:OneDriveConsumer/Backup";
-  "C:/Music"               = "$Env:OneDriveConsumer/Music";
-  "C:/Pictures"            = "$Env:OneDriveConsumer/Pictures";
-  "C:/Photos"              = "$Env:OneDriveConsumer/Photos";
-  "C:/Staging"             = "$Env:OneDriveConsumer/Staging";
-  "C:/Videos"              = "$Env:OneDriveConsumer/Videos";
-  "C:/Tools"               = "$Env:APPDATA/Ghisler/Tools";
-  "$Env:LOCALAPPDATA/nvim" = "$HOME/Projects/contento/dotfiles/nvim/.config/nvim";
+  "C:/Projects"              = "$Home/Projects";
+  "C:/Logs"                  = "$Home/Logs";
+  "C:/Temp"                  = "$Home/Temp";
+  "C:/Virtual Machines"      = "$Home/Virtual Machines";
+  "C:/Backup"                = "$Env:OneDriveConsumer/Backup";
+  "C:/Music"                 = "$Env:OneDriveConsumer/Music";
+  "C:/Pictures"              = "$Env:OneDriveConsumer/Pictures";
+  "C:/Photos"                = "$Env:OneDriveConsumer/Photos";
+  "C:/Staging"               = "$Env:OneDriveConsumer/Staging";
+  "C:/Videos"                = "$Env:OneDriveConsumer/Videos";
+  "C:/Tools"                 = "$Env:APPDATA/Ghisler/Tools";
+  "$Env:LOCALAPPDATA/nvim"   = "$HOME/Projects/contento/dotfiles/nvim/.config/nvim";
+  "$Home/Downloads/Material" = "$Env:OneDriveConsumer/Pictures/Material";
 }
 
 $LinkInfo.GetEnumerator() | ForEach-Object {
   $path = $_.Key
   $target = $_.Value
+  Write-Output "**** [$target - directory] Creating ..."
   New-Item -ItemType Directory -Path $target -ErrorAction Continue
+  Write-Output "     [$target - link] Creating from $path to $target"
   New-Item -ItemType SymbolicLink -Path $path -Target $target -ErrorAction Continue
 }
-
 # Map drive A: to \\tesoro\assets
+Write-Output "**** [A: - link] Creating from A: to \\tesoro\assets"
 subst A: "\\tesoro\assets"
-
-##############################################################
-# Windows Apps - Links
-
-$WINDOWS_APPS = "$Env:LOCALAPPDATA\Microsoft\WindowsApps"
-
-# Python
-# Get Python version
-$pythonVersionFull = & python --version 2>&1
-$pythonVersionNumbers = $pythonVersionFull.Split(' ')[1]
-$pythonMajorVersion = $pythonVersionNumbers.Split('.')[0]
-$pythonMinorVersion = $pythonVersionNumbers.Split('.')[1]
-$pythonVersion = "Python$pythonMajorVersion$pythonMinorVersion"
-
-# Build the path
-# if you're use LocalCache
-# $packageName = "PythonSoftwareFoundation.Python.$pythonMajorVersion.$($pythonMinorVersion)_qbz5n2kfra8p0"
-# $PYTHON_SCRIPTS_PATH = "$env:LOCALAPPDATA\Packages\$packageName\LocalCache\local-packages\$pythonVersion\Scripts"
-# or if you're using Programs
-$PYTHON_SCRIPTS_PATH = "$Env:LOCALAPPDATA/Programs/Python/$pythonVersion/Scripts "
