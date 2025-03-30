bcf8cd16ccf78a24a58eb9267fa0bf38d2faef5e
Gonzalo Contento <3741250+contento@users.noreply.github.com>
2025-03-30 18:59:50 -0400

    Add drive P:, also remove existing link if there is one

--- New-SymbolicLinks.ps1
+++ New-SymbolicLinks.ps1
@@ -1,5 +1,17 @@
 #requires -RunAsAdministrator
 
+# Ensure P: drive exists
+if (-not (Test-Path -Path "P:\")) {
+  Write-Output "Drive P: does not exist. Creating it..."
+  $projectsPath = "$Home/Projects"
+  if (-not (Test-Path -Path $projectsPath)) {
+    Write-Output "Creating directory $projectsPath..."
+    New-Item -ItemType Directory -Path $projectsPath -ErrorAction Stop
+  }
+  New-PSDrive -Name "P" -PSProvider "FileSystem" -Root $projectsPath -Persist
+  Write-Output "Drive P: created and mapped to $projectsPath."
+}
+
 # To Folders
 $LinkInfo = @{
   "C:/Backup"                = "$Env:OneDriveConsumer/Backup";
@@ -7,15 +19,11 @@ $LinkInfo = @{
   "C:/Music"                 = "$Env:OneDriveConsumer/Music";
   "C:/Photos"                = "$Env:OneDriveConsumer/Photos";
   "C:/Pictures"              = "$Env:OneDriveConsumer/Pictures";
-  "C:/Projects"              = "$Home/Projects";
+  "C:/Projects"              = "P:/";
   "C:/Staging"               = "$Env:OneDriveConsumer/Staging";
   "C:/Videos"                = "$Env:OneDriveConsumer/Videos";
-  # "C:/Virtual Machines"      = "$Home/Virtual Machines";
   "C:/Temp"                  = "$Home/Temp";
   "C:/Tools"                 = "$Env:APPDATA/Ghisler/Tools";
-  #
-  # "$Env:LOCALAPPDATA/nvim"   = "$HOME/Projects/contento/dotfiles/nvim/.config/nvim";
-  #
   "$Home/Downloads/Assets"   = "\\tesoro\Assets";
   "$Home/Downloads/Material" = "$Env:OneDriveConsumer/Pictures/Material";
   "$Home/Downloads/Temp"     = "$Home/Temp";
@@ -25,8 +33,16 @@ $LinkInfo = @{
 $LinkInfo.GetEnumerator() | ForEach-Object {
   $path = $_.Key
   $target = $_.Value
-  Write-Output "**** [$target - directory] Creating ..."
-  New-Item -ItemType Directory -Path $target -ErrorAction Continue
+
+  if (-not (Test-Path -Path $target)) {
+    Write-Output "**** [$target - directory] Creating ..."
+    New-Item -ItemType Directory -Path $target -ErrorAction Continue
+  }
+
+  if (Test-Path -Path $path) {
+    Write-Output "     [link] Removing existing link at $path"
+    Remove-Item -Path $path -Force -Recurse -ErrorAction Continue
+  }
   Write-Output "     [$target - link] Creating from $path to $target"
   New-Item -ItemType SymbolicLink -Path $path -Target $target -ErrorAction Continue
 }
